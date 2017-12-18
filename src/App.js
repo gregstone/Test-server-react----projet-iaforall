06df759abf57c955996ef0f9046743747002acd3
jeremy d <jeremy.david94@gmail.com>
2017-12-18 16:01:11 +0100

    Ajout du module pour tager les mots, A terminer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import TagReact from './TagReact';
+
+// Composant permettant d'afficher sur la page les phrases saisies sous forme de liste 
 import List from './List';
 
-class App extends Component {
-  constructor(props){
-    super(props);
-    //this.state = {value: ''};
-    this.state = {
-        term: '',   //   term to store what we passing as a value to our input
-        items: []   //   items to store every value which we passing to our todo list
-      };
-    }
+// module 'react-tag-input' pour tagger les mots 
+import { WithContext as ReactTags } from 'react-tag-input';
 
-  onChange = (event) => {
-      this.setState({term: event.target.value});
+// Feuille de style associée au module 'react-tag-input' 
+import './react-tags.css';
+
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+
+// Valeur de départ permettant l'ajout de phrases et le stockage de celles ci 
+//sous forme de liste 
+      term: '',   //   term to store what we passing as a value to our input
+      items: [],   //   items to store every value which we passing to our todo list
+      
+// Valeurs par défault du champs permettant l'ajout de tags 
+      tags: [{ id: 1, text: "Thailand" }, { id: 2, text: "India" }] 
+    };
+
+    this.handleDelete = this.handleDelete.bind(this);
+    this.handleAddition = this.handleAddition.bind(this);
+    this.handleDrag = this.handleDrag.bind(this);
+
   }
-  
+
+///////// Evenements associés au champs input pour l'ajout de phrases //////////
+
+  // Stocke la valeur saisie par l'utilisateur dans 'term'
+  onChange = (event) => {
+    this.setState({ term: event.target.value });
+  }
+
+  // Lors de la soumission, affiche la phrase saisie sous forme 
+  // de liste sous le champs input
   onSubmit = (event) => {
     event.preventDefault();
     this.setState({
@@ -25,13 +49,47 @@ class App extends Component {
       items: [...this.state.items, this.state.term]
     });
     console.log("toto");
-    console.log([this.state.items[1], this.state.items[2] ]);
+    console.log([this.state.items[1], this.state.items[2]]);
   }
 
-render() {
+
+///////// Evenements associés au module react-tag-input //////////
+
+  // Suppresion de tags
+  handleDelete(i) {
+    let tags = this.state.tags;
+    tags.splice(i, 1);
+    this.setState({ tags: tags });
+  }
+
+  // Ajout de tags
+  handleAddition(tag) {
+    let tags = this.state.tags;
+    tags.push({
+      id: tags.length + 1,
+      text: tag
+    });
+    this.setState({ tags: tags });
+  }
+
+  // Déplacement de tags
+  handleDrag(tag, currPos, newPos) {
+    let tags = this.state.tags;
+
+    // mutate array 
+    tags.splice(currPos, 1);
+    tags.splice(newPos, 0, tag);
+
+    // re-render 
+    this.setState({ tags: tags });
+  }
+
+
+  render() {
+    const { tags, suggestions } = this.state;
     return (
-        
-      <div className="App">  
+
+      <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
@@ -42,56 +100,33 @@ render() {
 
 
         <form className="App" onSubmit={this.onSubmit}>
-            
-            {/* onSubmit is a function which does three things:    
-                --> Cleans the input field after a submit action is triggered, by resetting 
-                term to the initial empty string value
-                --> Pushes every term to our array of items after submit
-                --> The preventDefault() method stops the default action of an element 
-                    from happening. */}
-            
-            <input value={this.state.term} onChange={this.onChange} />
-            {/* value takes what is stored in our state as term */}
-            {/* onChange is a function which changes our state depending on the current input 
-            value. To change state we need to use the this.setState() method which also triggers 
-            UI update. */}
-            
-            <button>Submit</button>
-        </form> 
-
-        <List items={this.state.items}/>
 
 
+          {/* CAHMPS INPUT POUR LA SAISIE DE PHRASES */}
+          <input value={this.state.term} onChange={this.onChange} />
           
+          <button>Submit</button>
+        </form>
 
-      </div>       
+        {/* CHAMPS INPUT POUR AJOUTER DES TAGS  */}
+        <ReactTags  tags={tags}
+                    suggestions={suggestions}
+                    handleDelete={this.handleDelete}
+                    handleAddition={this.handleAddition}
+                    handleDrag={this.handleDrag} />
         
+        <ReactTags  tags={tags}
+                    suggestions={suggestions}
+                    handleDelete={this.handleDelete}
+                    handleAddition={this.handleAddition}
+                    handleDrag={this.handleDrag} />
+        
+        {/* COMPOSANT QUI AFFICHE LES PHRASES SAISIES SOUS FORME DE LISTE SOUS LE CHAMPS INPUT  */}
+        <List items={this.state.items} />
+
+      </div>
     )
+  }
 }
-
-
-
-
-}  
-
-//   render() {
-//     return (
-//       <div className="App">
-//         <header className="App-header">
-//           <img src={logo} className="App-logo" alt="logo" />
-//           <h1 className="App-title">Welcome to React</h1>
-//         </header>
-//         <p className="App-intro">
-//           To get started, edit <code>src/App.js</code> and save to reload.
-//         </p>
-        
-        
-        
-        
-//         <TagReact />
-//       </div>
-//     );
-//   }
-// }
 
 export default App;
